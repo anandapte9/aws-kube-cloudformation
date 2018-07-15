4e04e7ce4745bd3a6c93a12a93b331a6355db217
Anand Apte <anandapte9@gmail.com>
2018-07-15 18:45:07 +1000

    My first commit for AWS cloudformation template for basic kubernetes cluster.

---
+++ files/nodes/kube-configure.sh
@@ -0,0 +1,33 @@
+sudo amazon-linux-extras install docker -y
+sudo usermod -a -G docker ec2-user
+cat <<EOF > /home/ec2-user/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=0
+gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOF
+cd /etc/yum.repos.d
+sudo cp /home/ec2-user/kubernetes.repo .
+cat <<EOF >  /home/ec2-user/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+cd /etc/sysctl.d
+sudo mv /home/ec2-user/k8s.conf .
+sudo sysctl --system
+sudo setenforce 0
+sudo yum install kubelet kubeadm kubectl -y
+sudo systemctl enable docker
+sudo systemctl start docker
+cd /etc/systemd/system/kubelet.service.d/
+sudo sed -i "s/cgroup-driver=systemd/cgroup-driver=cgroupfs/g" /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+sudo touch 90-local-extras.conf
+sudo chmod 777 90-local-extras.conf
+sudo echo 'Environment="KUBELET_EXTRA_ARGS=--fail-swap-on=false"' > /etc/systemd/system/kubelet.service.d/90-local-extras.conf
+sudo chmod 755 90-local-extras.conf
+sudo swapoff -a
+sudo systemctl daemon-reload
+sudo systemctl enable kubelet
