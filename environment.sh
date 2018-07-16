fc8ec997da7397acbedc3cddc36152ed87054296
Anand Apte <anandapte9@gmail.com>
2018-07-17 07:53:30 +1000

    updated environment.sh with user specified region

--- environment.sh
+++ environment.sh
@@ -3,5 +3,9 @@
 echo 'Enter a name for your S3 Bucket..'
 read BUCKET_NAME
 
+echo 'Enter the region you are cnfiguring your stack in (same region your AWS CLI is configred to..)'
+read REGION
+
 sed 's/BUCKET_NAME/'$BUCKET_NAME'/g' parameters/parameters-example0.json > parameters.json
 sed 's/BUCKET_NAME/'$BUCKET_NAME'/g' commands/commands.txt > commands.txt
+sed 's/ap-southeast-2/'$REGION'/g' commands/commands.txt > commands.txt
