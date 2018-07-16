74b04c16332d61ac388c9915e1bf184715a59473
Apte <anand.apte@accenture.com>
2018-07-16 17:18:24 +1000

    added environment configuration script

--- environment.sh
+++ environment.sh
@@ -3,8 +3,5 @@
 echo 'Enter a name for your S3 Bucket..'
 read BUCKET_NAME
 
-echo 'Enter the region for your cloud formation stack'
-read REGION
-
 sed 's/BUCKET_NAME/'$BUCKET_NAME'/g' parameters/parameters-example0.json > parameters.json
 sed 's/BUCKET_NAME/'$BUCKET_NAME'/g' commands/commands.txt > commands.txt
